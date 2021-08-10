b86542eafda5b9a9f77f5c843f27d0f7aa37a27a
Triana Cerda <trianaRcerda@gmail.com>
2021-08-10 14:32:34 -0700

    passing test with numberified find by id

---
+++ __test__/get.test.js
@@ -0,0 +1,19 @@
+const app = require("../app.js");
+const supertest = require("supertest");
+const request = supertest(app);
+
+const data = require("../data.js");
+
+it("gets the animals endpoint", async () => {
+  const response = await request.get("/animals");
+
+  expect(response.status).toBe(200);
+  expect(response.body).toEqual(data);
+});
+
+it("gets the anima show endpoint", async () => {
+  const response = await request.get("/animals/1");
+
+  expect(response.status).toBe(200);
+  expect(response.body).toEqual(data[0]);
+});
---
+++ data.js
@@ -0,0 +1,20 @@
+const data = [
+  {
+    id: 1,
+    name: "Tala",
+  },
+  {
+    id: 2,
+    name: "Tony",
+  },
+  {
+    id: 3,
+    name: "Bella",
+  },
+  {
+    id: 4,
+    name: "Luna",
+  },
+];
+
+module.exports = data;
