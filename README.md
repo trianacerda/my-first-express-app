8e7704168d0651791be8f7e7caee7a24d77cc843
Triana Cerda <trianaRcerda@gmail.com>
2021-08-10 11:58:02 -0700

    added index.js to the back end

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # my-first-express-app
+
+- Create a new repo in Github with README and clone it down `git clone <URL>`
+- Open code in VSCode
+- Run `npm init -y` --creates a `package.json` file -- go ahead and ACP
+- Install Express App dependencies with `npm i express supertest jest cors`
+- Add a .gitignore and add `node_modules`
---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("hello you!");
+//imports are different in Node
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
