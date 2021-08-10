7038ebd443069ed317b86c69176ed302f50a655f
Triana Cerda <trianaRcerda@gmail.com>
2021-08-10 15:40:36 -0700

    added more categories for animals data

--- data.js
+++ data.js
@@ -2,18 +2,26 @@ const data = [
   {
     id: 1,
     name: "Tala",
+    type: "dog",
+    snuggly: true,
   },
   {
     id: 2,
     name: "Tony",
+    type: "cat",
+    snuggly: true,
   },
   {
     id: 3,
     name: "Bella",
+    type: "cat",
+    snuggly: true,
   },
   {
     id: 4,
     name: "Luna",
+    type: "dog",
+    snuggly: true,
   },
 ];
 
